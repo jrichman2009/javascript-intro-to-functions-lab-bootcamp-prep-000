28176b7b8328a3a9f19baa2f77e0912743664e77
Jeffrey Richman <jrichman2009@gmail.com>
2018-08-18 21:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  return string.toUpperCase
+}
+
+function logWhispher(string) {
+  return string.toLowerCase
+}
+
+function sayHiToGrandma(string) {
+  if 
+}
